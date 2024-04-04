6778c4f025083e6742f7dc2366a897bb8dc93179
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-04-04 09:02:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "9inex3" //ur yt chanel name
+global.socialm = "Insta: @false_eye" //ur github or insta name
+global.location = "India,Kochi" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.botname = '9ine.x3' //ur bot name
+global.ownernumber = '919539901834' //ur owner number
+global.ownername = '𝒇𝒂𝒍𝒔𝒆 𝒆𝒚𝒆' //ur owner name
+global.websitex = "https://instagram.com/false_eye"
+global.wagc = "https://t.me/sarca_stics"
+global.themeemoji = '💤'
+global.wm = "9ine.x3"
+global.botscript = 'https://github.com/false-eye' //script link
+global.packname = "9ine.x3"
+global.author = "@false_eye"
+global.creator = "919539901834@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["919539901834"] // Premium User
 global.hituet = 0
 
 //bot sett
